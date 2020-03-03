f081d244219849c4c3bd3d610f2fd84b4021d845
Ryuichiro Hara <hara.ryuichiro@gmail.com>
2020-03-03 15:35:07 +0900

    -

---
+++ tests/test2.py
@@ -0,0 +1,60 @@
+import os
+import pandas as pd
+import bindlib as bl
+import prody
+
+def get_catalog():
+    df = pd.read_csv('../cbi_prep/data.csv')
+    D = {}
+    for i in df.index:
+        r = df.loc[i]
+        pdbid = r['pdbid']
+        ligname = r['ligname']
+        D[pdbid] = ligname
+    return D
+
+def gen():
+    for root, dirs, files in os.walk('../cbi_prep/data'):
+        for f in files:
+            if f.endswith('.pdb.gz') and not f.endswith('.apo.pdb.gz'):
+                fname = f'{root}/{f}'
+                yield fname
+
+cat = get_catalog()
+to_smi = bl.AtomGroupConv(to='smi')
+
+thres = 5.0
+
+for fname in gen():
+    pdbid = os.path.basename(fname)[:4]
+
+    ligname = cat.get(pdbid)
+    if ligname is None:
+        print(pdbid, f'Error: no ligname entry')
+        continue
+    apo = bl.AtomGroupPDBReader('protein and not hydrogen')(fname)
+    hetero = bl.AtomGroupPDBReader('hetero and not hydrogen and not water')(fname)
+    if hetero is None:
+        print(pdbid, f'Error: no appropriate hetero entry in PDB')
+        continue
+
+    ligand = bl.pick_ligand(hetero, ligname)
+
+    if ligand is None:
+        print(pdbid, f'Error: no ligand "{ligname}"')
+        continue
+
+    chains = bl.get_contact_chains(apo, ligand)
+    if chains is None:
+        print(pdbid, f'Error: no contact with protein "{pdbid}" and {ligand.getTitle()}')
+        continue
+
+    pocket = bl.get_pocket_residues(apo, ligand)
+
+    if pocket is None:
+        print(pdbid, f'Error: could not find pocket of {ligand.getTitle()} in "{pdbid}"')
+        continue
+
+    print(pdbid, ligand.getTitle(), ligand.numAtoms(), apo.numAtoms(), '->', chains.numAtoms(), '->', pocket.numAtoms())
+
+    # prody.writePDB(f'{pdbid}.apo.pdb.gz', chains)

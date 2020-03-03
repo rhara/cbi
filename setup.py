5a47ce11bf722a4e6bcce2c50b5440006c678a24
Ryuichiro Hara <hara.ryuichiro@gmail.com>
2020-03-03 11:21:07 +0900

    -

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bindlib',
-    version='0.0.4',
+    version='0.0.5',
     description='ProDy-OpenBabel-RDKit triad',
     author='Ryuichiro Hara',
     author_email='hara.ryuichiro@gmail.com',

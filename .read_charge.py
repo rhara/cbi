c3d4b70e115c80fb8818d29abcc87f41d590b144
Ryuichiro Hara <hara.ryuichiro@gmail.com>
2020-03-03 18:21:25 +0900

    -

--- .read_charge.py
+++ .read_charge.py
@@ -1,13 +1,11 @@
 import sys
 import openbabel
+import numpy as np
 
 status = False
 
-iname = sys.argv[1]
-oname = sys.argv[2]
-
-out = open(oname, 'wt')
-for line in open(iname, 'rt'):
+vs = []
+for line in open('tmp/89c61fbcd4ed45d48630a98e35eb60ff.tmpdir/protein.mol2', 'rt'):
     line = line.rstrip()
     if line.startswith('@<TRIPOS>ATOM'):
         status = True
@@ -18,6 +16,29 @@ for line in open(iname, 'rt'):
     if not status:
         continue
     it = line.split()
-    print(it[8], file=out)
+    vs.append(float(it[8]))
 
+vs = np.array(vs)
+print(vs)
 
+conv = openbabel.OBConversion()
+conv.SetInAndOutFormats('.pdb', '.mol2')
+
+mol = openbabel.OBMol()
+conv.ReadFile(mol, 'tmp/89c61fbcd4ed45d48630a98e35eb60ff.tmpdir/protein_H.pdb')
+conv.WriteFile(mol, 'out1.mol2')
+mol.UnsetPartialChargesPerceived()
+for d in dir(mol):
+    print(d)
+
+for i in range(mol.NumAtoms()):
+    atom = mol.GetAtomById(i)
+    print(atom.GetAtomicNum(), atom.GetPartialCharge(), vs[i])
+    atom.SetPartialCharge(vs[i])
+
+# mol.SetPartialChargesPerceived()
+cont = conv.WriteString(mol).rstrip().split('\n')
+cont[1] = 'PROTEIN'
+cont[3] = 'PROTEIN'
+cont[4] = 'USER_CHARGES'
+print('\n'.join(cont))

935dbe31bb3cbd398604469c2dd2c29ba4e02b8f
Ryuichiro Hara <hara.ryuichiro@gmail.com>
2020-03-03 09:28:01 +0900

    -

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ import os
 from setuptools import setup, find_packages
 
 setup(
-    name='proobr',
+    name='bindlib',
     version='0.0.3',
     description='ProDy-OpenBabel-RDKit triad',
     author='Ryuichiro Hara',
     author_email='hara.ryuichiro@gmail.com',
-    url='https://github.com/rhara/proobr',
+    url='https://github.com/rhara/bindlib',
     license='no license',
     include_package_data=True,
     packages=find_packages(exclude=('tests', 'docs')),

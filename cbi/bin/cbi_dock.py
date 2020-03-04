dc86d106bf88aa498fdaf84a5e5729db160412da
Ryuichiro Hara <hara.ryuichiro@gmail.com>
2020-03-04 12:07:23 +0900

    -

--- cbi/bin/cbi_dock.py
+++ cbi/bin/cbi_dock.py
@@ -7,10 +7,12 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('apo_iname', type=str)
     parser.add_argument('mol_iname', type=str)
+    parser.add_argument('--quiet', '-q', action='store_true')
     args = parser.parse_args()
 
     apo_iname = args.apo_iname
     mol_iname = args.mol_iname
+    quiet = args.quiet
 
     pdbid = os.path.basename(apo_iname)[:4]
 
@@ -18,8 +20,9 @@ def main():
     workdir = tleap(apo_iname)
     protein_fname = f'{workdir}/{pdbid}_protein_H_charged.mol2'
     ncpu = max(os.cpu_count()-2, 1)
-    cbi.smina_dock(protein_fname, mol_iname, ncpu=ncpu)
-    print(f'{workdir}/{os.path.basename(mol_iname)[:-4]}_docked.sdf')
+    cbi.smina_dock(protein_fname, mol_iname, ncpu=ncpu, quiet=quiet)
+    if not quiet:
+        print(f'write to {workdir}/{os.path.basename(mol_iname)[:-4]}_docked.sdf')
 
     ### cleaning
     for f in os.listdir(workdir):
--- cbi/dock.py
+++ cbi/dock.py
@@ -1,7 +1,7 @@
 import subprocess as sp
 import os
 
-def smina_dock(protein_fname, ligand_fname, ncpu=None, num_modes=4, seed=0):
+def smina_dock(protein_fname, ligand_fname, ncpu=None, num_modes=4, seed=0, quiet=False):
     basedir = os.path.dirname(__file__)
     workdir = os.path.dirname(os.path.abspath(protein_fname))
     pdbid = os.path.basename(protein_fname)[:4]
@@ -11,6 +11,11 @@ def smina_dock(protein_fname, ligand_fname, ncpu=None, num_modes=4, seed=0):
     logname = f'{workdir}/{os.path.basename(ligand_fname)[:-4]}.smina.log'
     center_script = f'{basedir}/etc/center.py'
     boxpars = sp.check_output(f'python {center_script} {ligand_fname}', shell=True).decode().strip()
-    sp.call(f'smina -r {protein_fname} -l {ligand_fname} {boxpars} --cpu {ncpu} --num_modes {num_modes} --seed {seed} -o {oname} --log {logname}', shell=True)
+    command = f'smina -r {protein_fname} -l {ligand_fname} '\
+              f'{boxpars} --cpu {ncpu} --num_modes {num_modes} --seed {seed} -o {oname} --log {logname}'
+    if quiet:
+        sp.call(command, shell=True, stderr=sp.DEVNULL, stdout=sp.DEVNULL)
+    else:
+        sp.call(command, shell=True)
     return workdir
 

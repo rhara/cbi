bcdda9aba3ab5f4a6c47bf3e6557d83955104c08
Ryuichiro Hara <hara.ryuichiro@gmail.com>
2020-03-04 13:30:05 +0900

    -

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ NOTE: This is going to be chained.
 ```
 pip install .
 
-$ cbi_prep example_data/1mwt.apo.pdb.gz PNM
+$ cbi_prep example_data/1mwt.pdb.gz PNM
 $ cbi_dock example_data/1mwt.apo.pdb.gz example_data/1mwt_PNM.sdf
 $ cbi_rmsd example_data/1mwt_PNM.pdb example_data/1mwt_PNM.docked.sdf
 
--- bin/fetch_data.py
+++ bin/fetch_data.py
@@ -3,12 +3,45 @@
 import prody
 import os
 import pandas as pd
+import multiprocessing as mp
+import argparse
 
-os.makedirs('data', mode=0x755, exist_ok=True)
+datadir = None
 
-df = pd.read_csv('example_data/data.csv')
+def worker(args):
+    count, pdbid = args
+    os.makedirs(f'{datadir}/{pdbid}', mode=0o755, exist_ok=True)
+    os.chdir(f'{datadir}/{pdbid}')
+    prody.fetchPDB(pdbid)
+    return dict(count=count, pdbid=pdbid)
 
-for i in df.index:
-    r = df.loc[i]
-    if 'select' not in r['label'] or '2019' not in r['label']:
-        print(r['label'])
+def main():
+    global datadir
+    parser = argparse.ArgumentParser()
+    parser.add_argument('iname', type=str)
+    parser.add_argument('datadir', type=str)
+    args = parser.parse_args()
+
+    datadir = os.path.abspath(args.datadir)
+
+    os.makedirs(datadir, mode=0o755, exist_ok=True)
+
+    df = pd.read_csv(args.iname)
+
+    def gen():
+        count = 0
+        for i in df.index:
+            r = df.loc[i]
+            if not ('select' in r['label'] or '2019' in r['label']):
+                continue
+            count += 1
+            pdbid = r['pdbid']
+            yield count, pdbid
+
+    pool = mp.Pool(mp.cpu_count())
+    for ret in pool.imap_unordered(worker, gen()):
+        count, pdbid = ret['count'], ret['pdbid']
+        print(count, pdbid)
+
+if __name__ == '__main__':
+    main()

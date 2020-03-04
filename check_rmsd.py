bcd674725fe19f6a8769234ac0eba02ac90c367c
Ryuichiro Hara <hara.ryuichiro@gmail.com>
2020-03-05 00:00:53 +0900

    -

--- check_rmsd.py
+++ check_rmsd.py
@@ -1,11 +1,28 @@
+#!/usr/bin/env python
+
 import glob
 import subprocess as sp
 
 flist = glob.glob('data/*/*.docked.sdf')
 
+ng = 0
+count = 0
+good = 0
 for f in flist:
     pdbid = f[5:9]
     ligname = f[15:18]
     ref = f'data/{pdbid}/{pdbid}_{ligname}.pdb'
     fit = f'data/{pdbid}/{pdbid}_{ligname}.docked.sdf'
-    sp.call(f'cbi_rmsd {ref} {fit}', shell=True)
+    output = sp.check_output(f'cbi_rmsd {ref} {fit}', shell=True, universal_newlines=True)
+    lines = output.split('\n')
+    rmsd = None
+    if 1 <= len(lines) and lines[0].startswith('1'):
+        count += 1
+        rmsd = float(lines[0].split()[1])
+        if rmsd <= 2.0:
+            good += 1
+    else:
+        ng += 1
+    percent = round(good/count*100, 1)
+    if 0 < count:
+        print(f'{pdbid} {rmsd} count={count} good={good} (percent={percent}%) ng={ng}')

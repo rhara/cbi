3af851d1e629a1f8b2998ef795a04abc26083474
Ryuichiro Hara <hara.ryuichiro@gmail.com>
2020-03-04 12:15:04 +0900

    -

--- README.md
+++ README.md
@@ -5,9 +5,11 @@ NOTE: This is going to be chained.
 ```
 pip install .
 
-$ cbi_dock example_data/1mwt.apo.pdb.gz example_dat/1mwt_PNM.sdf
-tmp/669acb2ca0cf49ceaa662233d7b3f901.tmpdir/docked.sdf
-$ cbi_rmsd example_dat/1mwt_PNM.sdf tmp/669acb2ca0cf49ceaa662233d7b3f901.tmpdir/docked.sdf
+$ cbi_prep example_data/1mwt.apo.pdb.gz PNM
+$ cbi_dock example_data/1mwt.apo.pdb.gz example_data/1mwt_PNM.sdf
+$ cbi_rmsd example_data/1mwt_PNM.pdb example_data/1mwt_PNM.docked.sdf
 
 $ cbi_prep example_data/5eol.pdb.gz 5QO
+$ cbi_dock example_data/5eol.apo.pdb.gz example_data/5eol_5QO.sdf
+$ cbi_rmsd example_data/5eol.pdb example_data/5eol_5QO.docked.sdf
 ```
--- cbi/bin/cbi_prep.py
+++ cbi/bin/cbi_prep.py
@@ -11,12 +11,14 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('pdb_iname', type=str)
     parser.add_argument('ligand_name', type=str)
-    parser.add_argument('--pocket-thres', type=float, default=5.0)
+    parser.add_argument('--pocket-thres', '-p', type=float, default=5.0)
+    parser.add_argument('--quiet', '-q', action='store_true')
     args = parser.parse_args()
 
     pdb_iname = args.pdb_iname
     ligand_name = args.ligand_name
     pocket_thres = args.pocket_thres
+    quiet = args.quiet
 
     basedir = os.path.abspath(os.path.dirname(pdb_iname))
     pdbid = os.path.basename(pdb_iname)[:4]
@@ -34,7 +36,7 @@ def main():
         print(pdbid, f'Error: {ligand_name} not found', file=sys.stderr)
         sys.exit(-1)
 
-    print(to_smi(ligand), ligand_name)
+    print(pdbid, to_smi(ligand), ligand_name)
 
     chains = cbi.get_contact_chains(protein, ligand)
     if chains is None:
@@ -42,21 +44,25 @@ def main():
         sys.exit(-1)
 
     chains_oname = f'{basedir}/{pdbid}.apo.pdb.gz'
-    print(f'write to {chains_oname}')
+    if not quiet:
+        print(f'write to {chains_oname}')
     prody.writePDB(chains_oname, chains)
 
     pocket = cbi.get_pocket_residues(chains, ligand)
 
     pocket_oname = f'{basedir}/{pdbid}.pocket_{pocket_thres}.pdb'
-    print(f'write to {pocket_oname}')
+    if not quiet:
+        print(f'write to {pocket_oname}')
     prody.writePDB(pocket_oname, pocket)
 
     ligand_block = to_sdf(ligand)
     ligand_oname = f'{basedir}/{pdbid}_{ligand_name}.sdf'
-    print(f'write to {ligand_oname}')
+    if not quiet:
+        print(f'write to {ligand_oname}')
     open(ligand_oname, 'wt').write(ligand_block)
     ligand_oname = f'{basedir}/{pdbid}_{ligand_name}.pdb'
-    print(f'write to {ligand_oname}')
+    if not quiet:
+        print(f'write to {ligand_oname}')
     prody.writePDB(ligand_oname, ligand)
 
 if __name__ == "__main__":

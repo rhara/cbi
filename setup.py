1ae4b2d254dc1d8539abaa8455d9d2488a103bc4
Ryuichiro Hara <hara.ryuichiro@gmail.com>
2020-03-05 01:57:13 +0900

    -

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cbi',
-    version='0.1.6',
+    version='0.2.0',
     description='ProDy-OpenBabel-RDKit triad',
     author='Ryuichiro Hara',
     author_email='hara.ryuichiro@gmail.com',
